de135785d6e4e305fe6d89345c5f9cc2e05e612b
kodecraft-herald <herald@kodecraft.dev>
2024-05-13 11:09:12 +0800

    Add: dotenvy_macro

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,3 +1,4 @@
+use dotenvy_macro::dotenv;
 use leptos::{html::Input, *};
 
 use crate::{models::login::{DirectusLoginRequest, DirectusLoginResponse}, utilities::{cookies::{set_jabra_cookie, JabraCookie}, errors::JabraError, http_wrapper::{call_and_parse, HttpMethod}}, HasError, Refetcher};
@@ -6,13 +7,7 @@ use crate::{models::login::{DirectusLoginRequest, DirectusLoginResponse}, utilit
 #[allow(non_snake_case)]
 #[component]
 pub fn Login() -> impl IntoView {
-    let url_env = move || {
-        if let Ok(data) = std::env::var("DIRECTUSURL") {
-            data
-        } else {
-            "No variable found.".to_string()
-        }
-    };
+    let url_env = dotenv!("DIRECTUSURL");
     view! {
         <div class="h-full lg:grid lg:grid-cols-3">
             <div class="h-full flex items-center justify-center px-4">
@@ -30,7 +25,7 @@ pub fn Login() -> impl IntoView {
                             <span class="font-bold">" digital assets"</span>
                         </h1>
                     </div>
-                    <p class="mt-4">{url_env()}</p>
+                    <p class="mt-4">{url_env}</p>
                     <p class="mt-4">A tailored solution for your investment thesis.</p>
                 </div>
             </div>
@@ -166,19 +161,8 @@ pub fn LoginIsland() -> impl IntoView {
 
 
 pub async fn directus_login(userid: String, password: String) -> Result<bool, ServerFnError> {
-    let url = move || {
-        if let Ok(data) = std::env::var("DIRECTUSURL") {
-            data
-        } else {
-            "No variable found.".to_string()
-        }
-    };
-    // let url = if let Ok(var) = std::env::var("DIRECTUSURL") {
-    //     var
-    // } else {
-    //     "".to_string()
-    // };
-    let path = format!("{}/auth/login", url());
+    let url = dotenv!("DIRECTUSURL");
+    let path = format!("{}/auth/login", url);
     let email = userid.clone();
     let login_request = DirectusLoginRequest::new(userid.into(), password.into());
     let response = call_and_parse::<DirectusLoginRequest, DirectusLoginResponse>(
